1fde45b436a5c3a524adb97d334898a359f9c13d
ZishJawaid <zisheanj@gmail.com>
2020-09-27 16:02:58 +0100

    Build: Added file upload controller

--- src/main/java/com/zish/instagram/Application.java
+++ src/main/java/com/zish/instagram/Application.java
@@ -1,4 +1,4 @@
-package com.makersacademy.acebook;
+package com.zish.instagram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
